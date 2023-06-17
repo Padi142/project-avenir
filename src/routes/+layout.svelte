758c2df20136af3f992f21d01b0616055a439185
Padias <matyas.krejza@seznam.cz>
2023-06-17 23:29:23 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css";
+  </script>
+  <nav>
+	<div class="flex justify-between ">
+		<a href="/" class="text-bold ml-10 mt-4">
+            Avenir
+		</a>
+		
+	</div>
+</nav>
+
+  <slot />
