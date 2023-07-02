ba9a6d88f8b10ea97020016d8b030ce6493a68ee
Matyáš Krejza <matyas.krejza@seznam.cz>
2023-07-02 02:43:48 +0200

    user hash shown

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -8,6 +8,8 @@
 <div class="flex flex-col gap-4 justify-center items-center">
 	<p class="my-12" />
 	<p class="text-5xl my-2" in:fade={{ delay: 500, duration: 500 }}>You found:</p>
-	<p class="text-2xl my-12" in:fade={{ delay: 1000, duration: 40 }}>1 code</p>
-	<p class="text-2xl my-12" in:fade={{ delay: 1000, duration: 40 }}>{data.user?.name}</p>
+	<p class="text-2xl my-12" in:fade={{ delay: 1000, duration: 400 }}>1 code</p>
+	<p class="text-2xl my-4" in:fade={{ delay: 1200, duration: 400 }}>{data.user?.name}</p>
+	<p class="desktop:text-3xl text-2xl my-4" in:fade={{ delay: 1200, duration: 400 }}>You'r login hash:</p>
+	<p class="desktop:text-2xl text-1xl my-4" in:fade={{ delay: 1200, duration: 400 }}>{data.user?.hash}</p>
 </div>
